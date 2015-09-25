f674eb7eb3be9bc3248de58359e5dc7580cfef77
Mg <gamem.8818@gmail.com>
2015-09-25 14:36:37 -0700

    update js

--- app.js
+++ app.js
@@ -16,47 +16,57 @@ app.set('port', process.env.PORT || 8000);
 app.use(morgan('combined'))
 
 
-app.get('/',function(req,res){
-	res.render('index.html')
+app.get('/', function(req, res) {
+    res.render('index.html')
 });
 
 
-app.get('/getArtist', function(req,res){
-	var name = req.query.artistname;
-	spotifyApi.searchArtists(name)
-  		.then(function(data) {
-    		res.setHeader('Content-Type', 'application/json');
-    		res.end(JSON.stringify(data.body.artists.items));
-  		}, function(err) {
-    		console.error(err);
-  		});
+app.get('/getArtist', function(req, res) {
+    var name = req.query.artistname;
+    spotifyApi.searchArtists(name)
+        .then(function(data) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(data.body.artists.items));
+        }, function(err) {
+            console.error(err);
+        });
 });
 
-app.get('/getRelatedArtists', function(req, res){
-  var id = req.query.artistid;
-  spotifyApi.getArtistRelatedArtists(id).then(function(data){
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(data.body.artists));
-  }, function(err) {
+app.get('/getRelatedArtists', function(req, res) {
+    var id = req.query.artistid;
+    spotifyApi.getArtistRelatedArtists(id).then(function(data) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data.body.artists));
+    }, function(err) {
         console.error(err);
-      });
+    });
 });
 
-app.get('/getArtistAlbums', function(req, res){
-  var id = req.query.artistid;
-  spotifyApi.getArtistAlbums(id).then(function(data){
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(data.body.items));
-  }, function(err){
-    console.error(err);
-  });
+app.get('/getArtistAlbums', function(req, res) {
+    var id = req.query.artistid;
+    spotifyApi.getArtistAlbums(id).then(function(data) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data.body.items));
+    }, function(err) {
+        console.error(err);
+        res.end(null);
+    });
 });
 
+app.get('/getArtistAlbums', function(req, res) {
+    var id = req.query.artistid;
+    spotifyApi.getArtistAlbums(id).then(function(data) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data.body.items));
+    }, function(err) {
+        console.error(err);
+    });
+});
 
 
 
 
 var httpListener = http.createServer(app);
-httpListener.listen(app.get('port'), function () {
+httpListener.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
 });
--- script/frontend.js
+++ script/frontend.js
@@ -1,56 +1,73 @@
-$(function(){
-	getArtistAlbums = function(artistId, callback){
-		$.ajax({
-                type: "GET",
-                url: "/getArtistAlbums",
-                dataType: 'json',
-                data: {
-                    'artistid': artistId
-                },
-                success: function(result) {
-                    callback(result);
-                },
-                error: function(error) {
-                    console.log(error)
-                    callback(null);
-                }
-            });
-	};
-	getArtist = function(artistName, callback){
-		console.log(artistName);
-		$.ajax({
-                type: "GET",
-                url: "/getArtist",
-                dataType: 'json',
-                data: {
-                    'artistname': artistName
-                },
-                success: function(result) {
-                    callback(result);
-                },
-                error: function(error) {
-                    console.log(error)
-                    callback(null);
-                }
-            });
-	};
-	getRelatedArtists = function(artistIds, callback){
-		$.ajax({
-                type: "GET",
-                url: "/getRelatedArtists",
-                dataType: 'json',
-                data: {
-                    'artistid': artistIds
-                },
-                success: function(result) {
-                    callback(result);
-                },
-                error: function(error) {
-                    console.log(error)
-                    callback(null);
-                }
-            });
-	}
+$(function() {
+    getArtistAlbums = function(artistId, callback) {
+        $.ajax({
+            type: "GET",
+            url: "/getArtistAlbums",
+            dataType: 'json',
+            data: {
+                'artistid': artistId
+            },
+            success: function(result) {
+                callback(result);
+            },
+            error: function(error) {
+                console.log(error)
+                callback(null);
+            }
+        });
+    };
+    getArtist = function(artistName, callback) {
+        console.log(artistName);
+        $.ajax({
+            type: "GET",
+            url: "/getArtist",
+            dataType: 'json',
+            data: {
+                'artistname': artistName
+            },
+            success: function(result) {
+                callback(result);
+            },
+            error: function(error) {
+                console.log(error)
+                callback(null);
+            }
+        });
+    };
+    getRelatedArtists = function(artistIds, callback) {
+        $.ajax({
+            type: "GET",
+            url: "/getRelatedArtists",
+            dataType: 'json',
+            data: {
+                'artistid': artistIds
+            },
+            success: function(result) {
+                callback(result);
+            },
+            error: function(error) {
+                console.log(error)
+                callback(null);
+            }
+        });
+    }
+    getTopTracks = function(artistIds, callback) {
+        $.ajax({
+            type: "GET",
+            url: "/getTopTracks",
+            dataType: 'json',
+            data: {
+                'artistid': artistIds
+            },
+            success: function(result) {
+                callback(result);
+            },
+            error: function(error) {
+                console.log(error)
+                callback(null);
+            }
+        });
+    }
 
 
 })
